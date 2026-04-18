149f272cd836afb95dc39794cea9de2a82207be3
Alex Fys <ahfys21644@users.noreply.github.com>
2026-04-18 22:13:36 +0800

    add get_version() helper and personal fork note

--- cognee/__init__.py
+++ cognee/__init__.py
@@ -1,5 +1,6 @@
 """Cognee - Knowledge graph and memory layer for AI applications."""
 
+# Personal fork - added get_version() helper and noted config import for easy access
 from cognee.api.v1.cognify import cognify
 from cognee.api.v1.add import add
 from cognee.api.v1.search import search
@@ -7,4 +8,9 @@ from cognee.api.v1.prune import prune
 from cognee.config import Config
 
 __version__ = "0.1.0"
-__all__ = ["cognify", "add", "search", "prune", "Config"]
+__all__ = ["cognify", "add", "search", "prune", "Config", "get_version"]
+
+
+def get_version() -> str:
+    """Return the current version of cognee."""
+    return __version__
