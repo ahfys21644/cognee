6b6b8cf64b1445c28ea6404676ad81d2489d8581
Alex Fys <ahfys21644@users.noreply.github.com>
2026-04-19 00:29:52 +0800

    Add get_version_tuple() helper for easier version comparisons

--- cognee/__init__.py
+++ cognee/__init__.py
@@ -1,6 +1,7 @@
 """Cognee - Knowledge graph and memory layer for AI applications."""
 
 # Personal fork - added get_version() helper and noted config import for easy access
+# Also added get_version_tuple() for easier version comparisons
 from cognee.api.v1.cognify import cognify
 from cognee.api.v1.add import add
 from cognee.api.v1.search import search
@@ -9,9 +10,14 @@ from cognee.config import Config
 
 __version__ = "0.1.0"
 __author__ = "topoteretes (fork)"
-__all__ = ["cognify", "add", "search", "prune", "Config", "get_version"]
+__all__ = ["cognify", "add", "search", "prune", "Config", "get_version", "get_version_tuple"]
 
 
 def get_version() -> str:
     """Return the current version of cognee."""
     return __version__
+
+
+def get_version_tuple() -> tuple:
+    """Return the current version as a tuple of ints, e.g. (0, 1, 0)."""
+    return tuple(int(x) for x in __version__.split("."))
